320a660633dcdf8ca78975acc52147d9c8e90f6f
Lazarus <git@devbeejohn.de>
2024-01-22 18:15:25 +0100

    Patches to make OGio compile.
    (cherry picked from commit 9b37401930efbe8ce187237913bde5091d7ceb03)
    (cherry picked from commit ac4929502458ead1d5b3319b6f9d0198a2b442fc)
    (cherry picked from commit fc4c9b04d725f4a9919312d9536b3b42495f0ed6)

--- src/OGSettingsBackend.h
+++ src/OGSettingsBackend.h
@@ -4,6 +4,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
+#define G_SETTINGS_ENABLE_BACKEND
+
+#include <gio/gsettingsbackend.h>
+#include <gio/gdesktopappinfo.h>
+#include <gio/gunixmounts.h>
+#include <gio/gdesktopappinfo.h>
+#include <gio/gfiledescriptorbased.h>
 #include <gio/gdesktopappinfo.h>
 #include <gio/gunixfdmessage.h>
 #include <gio/gunixinputstream.h>
