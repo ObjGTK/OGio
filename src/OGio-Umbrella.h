ce6e24b2ba6f9fab9445ead88d33cb971fe7486f
Lazarus <git@devbeejohn.de>
2024-09-27 20:50:57 +0200

    Remove ThreadedResolver from Umbrella header as well

--- src/OGio-Umbrella.h
+++ src/OGio-Umbrella.h
@@ -97,7 +97,6 @@
 #import "OGTcpWrapperConnection.h"
 #import "OGTestDBus.h"
 #import "OGThemedIcon.h"
-#import "OGThreadedResolver.h"
 #import "OGThreadedSocketService.h"
 #import "OGTlsCertificate.h"
 #import "OGTlsConnection.h"
